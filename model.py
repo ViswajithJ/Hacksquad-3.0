eed8b2b71d751b3a6ed105c1cc9a0aaf6b7c2cc6
Sooraj S <sooraj2642@gmail.com>
2022-06-25 21:53:38 +0530

    Update backend

--- model.py
+++ model.py
@@ -3,35 +3,23 @@
 import numpy as np
 import pandas as pd
 from sklearn.linear_model import LogisticRegression
-from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 import warnings
 import pickle
 warnings.filterwarnings("ignore")
 
-data = pd.read_csv("Forest_fire.csv")
+data = pd.read_csv("data.csv")
 data = np.array(data)
 
 X = data[1:, 1:-1]
 y = data[1:, -1]
 y = y.astype('int')
 X = X.astype('int')
-print(X,y)
+
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
 log_reg = LogisticRegression()
-lin_reg = LinearRegression(
-   fit_intercept = True, normalize = True, copy_X = True, n_jobs = 2
-)
-lin_reg.fit(X_train, y_train)
-
 
 log_reg.fit(X_train, y_train)
-
-inputt=[int(x) for x in "45 32 60".split(' ')]
-final=[np.array(inputt)]
-
-b = log_reg.predict_proba(final)
-print(b)
 
 pickle.dump(log_reg,open('model.pkl','wb'))
 model=pickle.load(open('model.pkl','rb'))
